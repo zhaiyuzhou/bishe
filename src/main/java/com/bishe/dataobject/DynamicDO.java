6530f13e796bb18288e81f57413051a3fdc9fe10
yuzhou1210 <1739688051@qq.com>
2024-04-17 18:24:32 +0800

    每日任务

---
+++ src/main/java/com/bishe/dataobject/DynamicDO.java
@@ -0,0 +1,44 @@
+package com.bishe.dataobject;
+
+import com.bishe.model.Dynamic;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDateTime;
+
+@Data
+public class DynamicDO {
+
+    private Long id;
+
+    private String content;
+
+    private String tag;
+
+    private String authorId;
+
+    private long likeNum;
+
+    private long transPondNum;
+
+    private LocalDateTime postedDate;
+
+    private LocalDateTime gmtCreated;
+
+    private LocalDateTime gmtModified;
+
+    public DynamicDO() {
+
+    }
+
+    public DynamicDO(Dynamic dynamic) {
+        BeanUtils.copyProperties(dynamic, this);
+    }
+
+    public Dynamic toModel() {
+        Dynamic dynamic = new Dynamic();
+        BeanUtils.copyProperties(this, dynamic);
+        return dynamic;
+    }
+
+}
